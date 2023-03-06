2e0b4a438ccfd012927c1c0d93dddb953c47bd0f
Jonathan Gutiérrez <jonathangutierrezperez@gmail.com>
2023-03-06 19:25:23 +0000

    añadida acción

--- src/app.js
+++ src/app.js
@@ -7,7 +7,8 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   let who = ["the dog", "my granma", "his turtle", "my bird"];
-  let what = ["eat", "pissed", "crushed", "broked"];
+  let action = ["eat", "pissed", "crushed", "broked"];
+  let what = ["my homework", "the keys", "the car", "my laptop"];
   let when = [
     "before the class",
     "right in time",
@@ -20,6 +21,8 @@ window.onload = function() {
 
   excusa +=
     who[Math.floor(Math.random() * who.length)] +
+    " " +
+    action[Math.floor(Math.random() * action.length)] +
     " " +
     what[Math.floor(Math.random() * what.length)] +
     " " +
